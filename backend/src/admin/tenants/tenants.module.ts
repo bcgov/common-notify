ba01dd2cb428dbf65ed90772dd8796045ee7e1a3
barrfalk <bfalk@salussystems.com>
2026-04-02 16:34:00 -0700

    setting up local environment

--- backend/src/admin/tenants/tenants.module.ts
+++ backend/src/admin/tenants/tenants.module.ts
@@ -4,11 +4,12 @@ import { Tenant } from './entities/tenant.entity'
 import { TenantsController } from './tenants.controller'
 import { TenantsService } from './tenants.service'
 import { KongModule } from '../../shared/kong/kong.module'
+import { JwtGuard } from '../../common/guards/jwt.guard'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tenant]), KongModule],
   controllers: [TenantsController],
-  providers: [TenantsService],
+  providers: [TenantsService, JwtGuard],
   exports: [TenantsService],
 })
 export class TenantsModule {}
