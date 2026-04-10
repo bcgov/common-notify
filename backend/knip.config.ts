d45b9d2e5918388a54959497728ebee5da4e01d6
barrfalk <bfalk@salussystems.com>
2026-04-09 20:18:49 -0700

    wip fixing tests

--- backend/knip.config.ts
+++
@@ -1,7 +0,0 @@
-import type { KnipConfig } from 'knip'
-
-const config: KnipConfig = {
-  project: ['src/**/*.ts', 'test/**/*.ts', 'e2e/**/*.ts', 'vitest.config.mts', 'nest-cli.json'],
-}
-
-export default config
