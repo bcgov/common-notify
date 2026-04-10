00251bf6abb88a0cfe47725febb597408da50076
mgtennant <100305096+mgtennant@users.noreply.github.com>
2026-04-10 12:01:06 -0700

    normalize ches folder structure with api/notify

--- backend/src/ches/ches.module.ts
+++ backend/src/ches/ches.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { ChesController } from './v1/ches.controller'
+import { ChesController } from './ches.controller'
 import { ChesOAuthService } from './ches-oauth.service'
 import { ChesApiClient } from './ches-api.client'
 import { AdminModule } from '../admin/admin.module'
