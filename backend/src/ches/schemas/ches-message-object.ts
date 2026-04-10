596971287f4400a6b8b279f9f62b0a90dd44cdc4
mgtennant <100305096+mgtennant@users.noreply.github.com>
2026-04-10 14:52:45 -0700

    Update ches-message-object.ts

--- backend/src/ches/schemas/ches-message-object.ts
+++ backend/src/ches/schemas/ches-message-object.ts
@@ -6,60 +6,60 @@ import {
   IsNumber,
   IsEnum,
   ValidateNested,
-} from 'class-validator';
-import { Type } from 'class-transformer';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+} from 'class-validator'
+import { Type } from 'class-transformer'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 
 export class ChesAttachmentObject {
-  @ApiPropertyOptional() @IsOptional() @IsString() content?: string;
-  @ApiPropertyOptional() @IsOptional() @IsString() contentType?: string;
+  @ApiPropertyOptional() @IsOptional() @IsString() content?: string
+  @ApiPropertyOptional() @IsOptional() @IsString() contentType?: string
   @ApiPropertyOptional({ enum: ['base64', 'binary', 'hex'] })
   @IsOptional()
   @IsEnum(['base64', 'binary', 'hex'])
-  encoding?: string;
-  @ApiPropertyOptional() @IsOptional() @IsString() filename?: string;
+  encoding?: string
+  @ApiPropertyOptional() @IsOptional() @IsString() filename?: string
 }
 
 export class ChesMessageObject {
-  @ApiProperty() @IsString() bodyType: string;
-  @ApiProperty() @IsString() body: string;
-  @ApiProperty() @IsEmail() from: string;
+  @ApiProperty() @IsString() bodyType: string
+  @ApiProperty() @IsString() body: string
+  @ApiProperty() @IsEmail() from: string
   @ApiProperty({ type: [String] })
   @IsArray()
   @IsEmail({}, { each: true })
-  to: string[];
-  @ApiProperty() @IsString() subject: string;
+  to: string[]
+  @ApiProperty() @IsString() subject: string
 
   @ApiPropertyOptional({ type: [ChesAttachmentObject] })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => ChesAttachmentObject)
-  attachments?: ChesAttachmentObject[];
+  attachments?: ChesAttachmentObject[]
 
   @ApiPropertyOptional({ type: [String] })
   @IsOptional()
   @IsArray()
   @IsEmail({}, { each: true })
-  bcc?: string[];
+  bcc?: string[]
 
   @ApiPropertyOptional({ type: [String] })
   @IsOptional()
   @IsArray()
   @IsEmail({}, { each: true })
-  cc?: string[];
+  cc?: string[]
 
-  @ApiPropertyOptional() @IsOptional() @IsNumber() delayTS?: number;
+  @ApiPropertyOptional() @IsOptional() @IsNumber() delayTS?: number
 
   @ApiPropertyOptional({ enum: ['base64', 'binary', 'hex', 'utf-8'] })
   @IsOptional()
   @IsEnum(['base64', 'binary', 'hex', 'utf-8'])
-  encoding?: string;
+  encoding?: string
 
   @ApiPropertyOptional({ enum: ['normal', 'low', 'high'] })
   @IsOptional()
   @IsEnum(['normal', 'low', 'high'])
-  priority?: string;
+  priority?: string
 
-  @ApiPropertyOptional() @IsOptional() @IsString() tag?: string;
+  @ApiPropertyOptional() @IsOptional() @IsString() tag?: string
 }
