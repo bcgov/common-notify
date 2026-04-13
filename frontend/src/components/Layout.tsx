38ec02d4ae1674faba9cabe6a401423f815b3c03
barrfalk <bfalk@salussystems.com>
2026-04-13 12:33:50 -0700

    SCSS Updates
    Created default styles to match BC Gov styles.
    Created layout.scss styles page

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,27 +1,40 @@
 import type { FC } from 'react'
+import { useEffect, useState } from 'react'
 import { Footer, Header } from '@bcgov/design-system-react-components'
 import { Link } from '@tanstack/react-router'
-import { Button } from 'react-bootstrap'
+import UserService from '@/service/user-service'
+import '@/scss/components/layout.scss'
 
 type Props = {
   children: React.ReactNode
 }
 
 const Layout: FC<Props> = ({ children }) => {
+  const [username, setUsername] = useState<string | null>(null)
+
+  useEffect(() => {
+    setUsername(UserService.getUsername() || null)
+  }, [])
+
+  const handleLogout = () => {
+    UserService.doLogout()
+  }
+
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <Header title={'Notify'}>
-        {' '}
-        <Link to="/">
-          <Button variant="light" size="lg">
-            <i className="bi bi-house-door-fill" />
-          </Button>
-        </Link>
+    <div className="layout-container">
+      <Header title={'Notify'} className="layout-header">
+        <div className="layout-header-nav">
+          <div className="layout-header-user">
+            {username && <span className="username">{username}</span>}
+            <button className="logout-button" onClick={handleLogout} title="Logout">
+              <i className="bi bi-box-arrow-right" />
+              <span>Logout</span>
+            </button>
+          </div>
+        </div>
       </Header>
-      <div className="d-flex flex-grow-1 align-items-start justify-content-center mt-5 mb-5 ml-1 mr-1">
-        {children}
-      </div>
-      <Footer />
+      <div className="layout-content">{children}</div>
+      <Footer className="layout-footer" />
     </div>
   )
 }
