c0e88bac08a369072e5ed44706ce79977a7d0511
mgtennant <100305096+mgtennant@users.noreply.github.com>
2026-05-05 16:11:28 -0700

    Update app.e2e-spec.ts

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import type { INestApplication } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { AppModule } from '../src/app.module'
 import { PendingNotificationRetryService } from '../src/queue/services/pending-notification-retry.service'
+import { NotificationPubSubService } from '../src/api/notification/notification-pubsub.service'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
@@ -27,6 +28,11 @@ describe('AppController (e2e)', () => {
       .useValue({
         onModuleInit: () => Promise.resolve(),
       })
+        .overrideProvider(NotificationPubSubService)
+        .useValue({
+          publish: () => Promise.resolve(),
+          onModuleDestroy: () => Promise.resolve(),
+        })
       .compile()
 
     app = moduleFixture.createNestApplication()
