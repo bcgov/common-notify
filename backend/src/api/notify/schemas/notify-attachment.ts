9a9a1a8496f8e25fa3a1c82a6c8c878e0d0b467f
Revanth-banala <revanth.banala@gov.bc.ca>
2026-05-20 23:45:20 -0700

    feat: add inline attachment DTOs to notify channels

---
+++ backend/src/api/notify/schemas/notify-attachment.ts
@@ -0,0 +1,16 @@
+import { IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+
+export class NotifyAttachment {
+  @ApiProperty()
+  @IsString()
+  filename: string
+
+  @ApiProperty()
+  @IsString()
+  mimeType: string
+
+  @ApiProperty()
+  @IsString()
+  data: string
+}
