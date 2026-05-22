41afa54fd300151c763b41709ca7ae2a88b4a98e
mgtennant <100305096+mgtennant@users.noreply.github.com>
2026-05-22 15:49:47 -0700

    Update templates.repository.ts

--- backend/src/api/templates/templates.repository.ts
+++ backend/src/api/templates/templates.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository, ILike } from 'typeorm'
 import { Template } from './entities/template.entity'
